4ae397d56f1260cc0a20e5325bea4587047b9335
Plemfistra2022 <iren.mezenina@gmail.com>
2026-03-14 15:38:23 +0200

    lessons/lesson1/map

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
+import {products} from "./data/productsList.ts";
+import MyProduct from "./components/my-product/MyProduct.tsx";
 
 function App() {
 
 
   return (
     <>
-    </>
+      {
+        products.map((product, index) => <MyProduct key={index} product={product}/>
+        )
+
+      }
+      </>
   )
 }
 
---
+++ src/data/productsList.ts
@@ -0,0 +1,24 @@
+import type {IProduct} from "../models/IProduct.ts";
+
+export const products: IProduct[] = [
+    {
+        title: "milk",
+        price: 22,
+        image: "src/img/milk.jpg",
+    },
+    {
+        title: "juice",
+        price: 27,
+        image: "src/img/juice.jpg",
+    },
+    {
+        title: "tomato",
+        price: 47,
+        image: "src/img/tomato.jpg",
+    },
+    {
+        title: "tea",
+        price: 15,
+        image: "src/img/tea.jpg",
+    }
+];
