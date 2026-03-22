821b8b74111b3949ce4e0414afa0adc65d836d7d
Plemfistra2022 <iren.mezenina@gmail.com>
2026-03-22 11:01:11 +0200

    homeworks/hw1/task4

--- src/components/course-card-component/CourseCardComponent.tsx
+++ src/components/course-card-component/CourseCardComponent.tsx
@@ -7,7 +7,7 @@ export const CourseCardComponent: FC<{course: ICourse}> = ({course}) => {
     return (
         <div className="course-card">
             <h2 className="course-title">{course.title}</h2>
-            <p className="course-info">{course.monthDuration} month & {course.hourDuration} hour</p>
+            <p className="course-info">{course.monthDuration} month <span>|</span> {course.hourDuration} hour</p>
             <ModulesComponent course={course} />
         </div>
     );
