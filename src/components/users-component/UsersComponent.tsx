3a69d83f05e2db150ec2867aa04399baf9f2db33
Plemfistra2022 <iren.mezenina@gmail.com>
2026-03-29 20:03:35 +0300

    lessons/lesson2/service-layer

---
+++ src/components/users-component/UsersComponent.tsx
@@ -0,0 +1,30 @@
+import {useEffect, useState} from "react";
+import type {IUser} from "../../models/IUser.ts";
+import {UserComponent} from "../user-component/UserComponent.tsx";
+import {getUsers} from "../../services/api.service.ts";
+
+export const UsersComponent = () => {
+    const [users, setUsers] = useState<IUser[]>([]);
+    useEffect(() => {
+        const fatchData = async () => {
+            const users = await getUsers();
+            setUsers(users);
+        }
+        fatchData();
+
+        return () => {
+            console.log("done");
+        }
+    }, [])
+
+    return (
+        <div>
+            {
+                users.map((value) => (
+                    <UserComponent key={value.id} user={value}/>
+                ))
+            }
+        </div>
+    );
+};
+
